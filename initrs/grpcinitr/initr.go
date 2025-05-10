ec09995733382955432c74ca3727b143b7de0f41
Farzin Far <farzinfweb@gmail.com>
2025-05-11 00:03:46 +0330

    feat: add grpc module

---
+++ initrs/grpcinitr/initr.go
@@ -0,0 +1,53 @@
+package grpcinitr
+
+import (
+	"context"
+
+	"github.com/47monad/apin/initr"
+	"github.com/47monad/apin/initropts"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/reflection"
+)
+
+type ServerShell struct {
+	Server       *grpc.Server
+	HealthServer *health.Server
+}
+
+var GrpcServer = initr.AgentFunc[*ServerStore, *ServerShell](initGrpcServer)
+
+func initGrpcServer(ctx context.Context, b initropts.Builder[*ServerStore]) (*ServerShell, error) {
+	store, err := b.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	shell := &ServerShell{}
+
+	shell.Server = grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			store.Interceptors...,
+		),
+	)
+
+	if store.HealthCheck {
+		shell.HealthServer = health.NewServer()
+		healthgrpc.RegisterHealthServer(shell.Server, shell.HealthServer)
+	}
+
+	if store.Runnable != nil {
+		store.Runnable(shell.Server)
+	}
+
+	if store.Reflection {
+		reflection.Register(shell.Server)
+	}
+
+	if store.PromMetrics != nil {
+		store.PromMetrics.InitializeMetrics(shell.Server)
+	}
+
+	return shell, nil
+}
---
+++ initrs/grpcinitr/opts.go
@@ -0,0 +1,91 @@
+package grpcinitr
+
+import (
+	"github.com/47monad/apin/internal/grpcutil"
+	"github.com/47monad/apin/internal/logger"
+	grpcprom "github.com/grpc-ecosystem/go-grpc-middleware/providers/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/grpc"
+)
+
+type ServerStore struct {
+	Interceptors []grpc.UnaryServerInterceptor
+	HealthCheck  bool
+	Reflection   bool
+	PromMetrics  *grpcprom.ServerMetrics
+	Runnable     func(*grpc.Server)
+}
+
+type ServerBuilder struct {
+	Opts []func(*ServerStore) error
+}
+
+func (b *ServerBuilder) Build() (*ServerStore, error) {
+	store := &ServerStore{}
+
+	for _, opt := range b.Opts {
+		if opt == nil {
+			continue
+		}
+
+		if err := opt(store); err != nil {
+			return nil, err
+		}
+	}
+
+	return store, nil
+}
+
+func (b *ServerBuilder) WithRunnable(runnable func(store *grpc.Server)) *ServerBuilder {
+	b.Opts = append(b.Opts, func(s *ServerStore) error {
+		s.Runnable = runnable
+		return nil
+	})
+	return b
+}
+
+func (b *ServerBuilder) WithHealthCheck() *ServerBuilder {
+	b.Opts = append(b.Opts, func(s *ServerStore) error {
+		s.HealthCheck = true
+		return nil
+	})
+	return b
+}
+
+func (b *ServerBuilder) SetPrometheus(reg *prometheus.Registry) *ServerBuilder {
+	b.Opts = append(b.Opts, func(s *ServerStore) error {
+		promInterceptor, metrics := grpcutil.WithPromMonitoring(reg)
+		s.PromMetrics = metrics
+		s.Interceptors = append(s.Interceptors, promInterceptor)
+		return nil
+	})
+	return b
+}
+
+func (b *ServerBuilder) SetLogging(l logger.Logger) *ServerBuilder {
+	b.Opts = append(b.Opts, func(b *ServerStore) error {
+		b.Interceptors = append(b.Interceptors, grpcutil.NewLoggingInterceptor(l))
+		return nil
+	})
+	return b
+}
+
+func (b *ServerBuilder) WithReflection() *ServerBuilder {
+	b.Opts = append(b.Opts, func(s *ServerStore) error {
+		s.Reflection = true
+		return nil
+	})
+	return b
+}
+
+func (b *ServerBuilder) AddInterceptor(i grpc.UnaryServerInterceptor) *ServerBuilder {
+	b.Opts = append(b.Opts, func(s *ServerStore) error {
+		s.Interceptors = append(s.Interceptors, i)
+		return nil
+	})
+	return b
+}
+
+func Opts() *ServerBuilder {
+	return &ServerBuilder{}
+}
